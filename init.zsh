3df824c085379269e1d30aef346afde7d967b226
Philip M. Gollucci <pgollucci@p6m7g8.com>
2019-09-20 17:52:20 -0400

    bootstrap

--- init.zsh
+++ init.zsh
@@ -5,9 +5,5 @@ p6df::modules::p6macosx::external::brew() { }
 p6df::modules::p6macosx::init() {
 
     local dir=$P6_DFZ_SRC_DIR/p6m7g8/p6macosx
-
-    local file
-    for file in $dir/lib/*.sh; do
-	 p6_file_load "$file"
-    done
+    p6_bootstrap "$dir"
 }
